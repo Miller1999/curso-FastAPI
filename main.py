e3f8bcaa2ee1bfa37af1355c20681c7c43864c07
Miller Arias <millerjavierarias@gmail.com>
2024-07-04 17:07:02 -0500

    Metodo get

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "Mi primera api con FastAPI"
 app.version = "0.0.1"
 
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
 @app.get("/",tags=["Home"])
 def message():
-  return "Hola mundo"
+  return HTMLResponse("<h1>Hello world</h1>")
+
+@app.get("/movies",tags=["Movies"])
+def get_movies():
+  return movies
