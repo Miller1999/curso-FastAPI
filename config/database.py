4c770e2f395f1ea2ff58a2abb03cecf7e270d8d5
Miller Arias <millerjavierarias@gmail.com>
2024-07-10 23:25:50 -0500

    Creacion de la base de datos

--- config/database.py
+++ config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 # Se instala sqlalchemy el cual nos permite conectarnos de manera sencilla a una base de datos y usar ORM
 # Se configura 
-sqlite_file_name = "database.sqlite"
+sqlite_file_name = "../database.sqlite"
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
 database_url = f"sqlite:///{os.path.join(base_dir,sqlite_file_name)}"
--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 from jwt_manager import create_token,validate_token
 from fastapi.security import HTTPBearer
+from config.database import session,engine,Base
+from models.movie import Movie
 
 app = FastAPI()
 app.title = "Mi primera api con FastAPI"
 app.version = "0.0.1"
+
+Base.metadata.create_all(bind=engine)
 
 # Clase para poder autenticar el acceso 
 class JWTBearer(HTTPBearer):
